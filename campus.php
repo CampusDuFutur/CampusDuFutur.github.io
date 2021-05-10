bb7e54e8df0ca438d4d80c923162b6045d16919d
CampusDuFutur <83952056+CampusDuFutur@users.noreply.github.com>
2021-05-10 15:44:06 +0200

    Add files via upload

---
+++ campus.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+    <link rel="stylesheet" href="style.css" />
+	<title>Campus v2</title>
+</head>
+<body>
+	<header>
+		<button
+			class = "Bouton_login"
+			type = "button"
+			onclick = "window.location.href='index.html';">
+			logout
+		</button>
+	</header>
+
+
+	<main>
+
+		<p>y'as tout pleins de trucs ici</p>
+
+	</main>
+
+
+	<footer>
+		<p>
+			Prevost Guillaume
+			Lhomme Léo
+			Moucquet Quentin
+			Dumez Pirlouit
+			Philippe-Taylor Corentin
+		</p>
+	</footer>
+
+
+</body>
+</html>
